8d2df139c2b13472b07f869e50ef7f242a23cd96
JunkLab <richard.ms.torres@gmail.com>
2024-04-11 20:00:38 -0300

    criação de arquivo .JS , ajustes no CSS e HTML

---
+++ Vallis/JS/login.js
@@ -0,0 +1,10 @@
+console.log ("agora chora")
+
+function submit__button () {
+        
+    var usuario = document.getElementById('username').value;
+    var senha = document.getElementById('password').value;
+        console.log("Usuário: " + username);
+        console.log("Senha: " + password);
+
+}
